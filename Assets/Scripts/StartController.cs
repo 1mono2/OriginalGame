1bf563e25ce62209022dc79d557a82552b576e73
MONO <taka19980618@gmail.com>
2021-06-12 14:23:37 +0900

    スタート画面を動的に

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -17,7 +17,7 @@ public class StartController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.anyKeyDown)
         {
             StartCoroutine(LoadGame());
         }
